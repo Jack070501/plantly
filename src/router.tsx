f4ec74e60a5c3a973f6237629502bc95a7382905
Jack Hicks <jhicks@ybs.co.uk>
2026-06-23 13:54:07 +0100

    Added not found page

--- src/router.tsx
+++ src/router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, createRoutesFromElements, Route } from "react-rout
 import { Home } from "./pages/home";
 import { Products } from "./pages/products";
 import { YourDetails } from "./pages/your-details";
+import { NotFound } from "./pages/not-found";
 
 export const router = createBrowserRouter (
     createRoutesFromElements(
@@ -9,6 +10,7 @@ export const router = createBrowserRouter (
             <Route index element={<Home/> } />
             <Route path="products" element={<Products/> } />
             <Route path="your-details" element={<YourDetails/> } />
+            <Route path="*" element= {<NotFound/> } />
         </Route>
     )
 )
